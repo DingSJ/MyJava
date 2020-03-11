4bfe437cea6452f7287e555e3339efd79a3b9c3c
DingSJ <321612621@qq.com>
2020-03-12 06:47:18 +0800

    Futrue Timeout Test

---
+++ javaBase/src/main/java/com/china/test/future/FutureTimeoutTest.java
@@ -0,0 +1,30 @@
+package com.china.test.future;
+
+import java.util.concurrent.*;
+
+public class FutureTimeoutTest {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+        Future<Integer> task = executorService.submit(()->{
+            System.out.println("Start...");
+            Thread.sleep(5_000);
+            System.out.println("End...");
+            return 1;
+        });
+
+        try {
+            Integer result = task.get(3, TimeUnit.SECONDS);
+//            Integer result = task.get();
+            System.out.println( "Result: " + result);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        executorService.shutdown();
+    }
+}
