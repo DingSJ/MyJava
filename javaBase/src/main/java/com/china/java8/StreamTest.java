cbbae679337136062e3c10edec19fe84ba7ffc7b
DingSJ <321612621@qq.com>
2020-03-24 08:57:05 +0800

    add java8 - transfer array list or file to Stream

--- javaBase/src/main/java/com/china/java8/StreamTest.java
+++ javaBase/src/main/java/com/china/java8/StreamTest.java
@@ -2,6 +2,9 @@ package com.china.java8;
 
 import org.junit.Test;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -9,6 +12,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Stream;
 
 /**
@@ -60,6 +64,63 @@ public class StreamTest {
     private Stream<String> buildListStream() {
         List<String> strList = Arrays.asList("aa","bb", "cc", "dd", "ee");
         return strList.stream();
+    }
+
+    @Test
+    public void testIteratorStream(){
+        Stream<Integer> arrStream = Stream.iterate(0, n -> n+2).limit(10);
+        System.out.println(arrStream);
+        arrStream.forEach(System.out::println);
+    }
+    @Test
+    public void testGenerateStream(){
+        Stream<Double> arrStream = Stream.generate(Math::random).limit(10);
+        System.out.println(arrStream);
+        arrStream.forEach(System.out::println);
+    }
+
+    @Test
+    public void testGenerateStream2(){
+        Stream<Random> arrStream = Stream.generate(Random::new).limit(10);
+        System.out.println(arrStream);
+        arrStream.forEach(System.out::println);
+    }
+
+
+    @Test
+    public void testFlatMapStream(){
+        String[] arr = {"afsafsv","sdffasdgfdg"};
+        Stream<String[]> stream = Arrays.stream(arr).map(w -> w.split(""));
+        Stream<String> stringStream = stream.flatMap(Arrays::stream);
+        stringStream.distinct().forEach(System.out::println);
+    }
+
+    @Test
+    public void testStrSpiltStream(){
+        String str = "1|^|2|^|3|^|4|^|5|^|6";
+        String[] split = str.split("\\|\\^\\|");
+        Arrays.stream(split).forEach(s -> System.out.println(s + " <->"));
+    }
+
+    @Test
+    public void testFileSpiltStream(){
+        try {
+            File write = new File("E:\\ideaSpace\\MyJava\\javaBase\\src\\main\\java\\com\\china\\java8\\TestWrite.txt");
+            BufferedWriter br = new BufferedWriter(new FileWriter(write));
+            Stream<String> lines = Files.lines(Paths.get("E:\\ideaSpace\\MyJava\\javaBase\\src\\main\\java\\com\\china\\java8\\FilterTest.java"));
+            lines.forEach(str -> {
+                try {
+                    br.write(str);
+                    br.newLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            br.close();
+            System.out.println("=========== 文件读写完成 ===========");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
