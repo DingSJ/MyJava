9f4f0ba3cb796df6ba7cb3881b40fe64093bf063
DingSJ <321612621@qq.com>
2020-03-28 18:37:10 +0800

    java 8 AppleFilterDemo - Predicate

--- javaBase/src/main/java/com/china/java8/inAction/AppleFilterTest4.java
+++ javaBase/src/main/java/com/china/java8/inAction/AppleFilterTest4.java
@@ -2,7 +2,10 @@ package com.china.java8.inAction;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
 
 /**
  * 策略模式
@@ -18,6 +21,39 @@ public class AppleFilterTest4 {
         List<Apple> filter2 = PredictFilter.filter(appleList, apple -> apple.getName().contains("C"));
         System.out.println(filter2);
 
+        testCreateApple();
+
+    }
+
+    private static void testCreateApple() {
+        BiFunction<String, Integer, Apple> appleBiFunction = Apple::new;
+        Apple apple = appleBiFunction.apply("DD",100);
+        System.out.println("Apple : " + apple);
+
+        Supplier<Apple> appleSupplier = Apple::new;
+        Apple app = appleSupplier.get();
+        System.out.println("App : "  + app);
+
+
+        // 1
+        Comparator<Apple> c1 = new Comparator<Apple>() {
+            @Override
+            public int compare(Apple o1, Apple o2) {
+                return o1.weight - o2.weight;
+            }
+        };
+
+        // 2
+        Comparator<Apple> c2 = (Apple o1, Apple o2) -> o1.weight - o2.weight;
+
+        // 3
+        Comparator<Apple> c3 = (o1,o2) -> o1.weight - o2.weight;
+
+        // 4
+        Comparator<Apple> c4 = Comparator.comparingInt(o -> o.weight);
+
+        // 5
+        Comparator<Apple> c = Comparator.comparing(Apple::getWeight);
     }
 
     private static List<Apple> normalFilter(List<Apple> appleList, ApplePredicate predicate) {
