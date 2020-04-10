47f6276afa21b5d7da40c8bb796acfa2dbdfbc78
DingSJ <321612621@qq.com>
2020-04-10 13:48:27 +0800

    java design pattern - builder

---
+++ javaBase/src/main/java/com/china/designPattern/builder/GoodbyeMessage.java
@@ -0,0 +1,10 @@
+package com.china.designPattern.builder;
+
+public class GoodbyeMessage extends AutoMessage{
+    /**
+     * 构造子
+     */
+    public GoodbyeMessage(){
+        System.out.println("发送欢送信息");
+    }
+}
---
+++ javaBase/src/main/java/com/china/designPattern/builder/WelcomeBuilder.java
@@ -0,0 +1,18 @@
+package com.china.designPattern.builder;
+
+public class WelcomeBuilder extends Builder {
+    public WelcomeBuilder() {
+        msg = new WelcomeMessage();
+    }
+
+    @Override
+    public void buildBody() {
+        msg.setBody("欢迎内容");
+    }
+
+    @Override
+    public void buildSubject() {
+        msg.setSubject("欢迎标题");
+    }
+
+}
